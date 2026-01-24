8e913576a52b445e7466eebdf096280c716fe101
JayV <66281743+Peliosso@users.noreply.github.com>
2026-01-24 11:45:26 -0300

    Criar config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define("BOT_TOKEN", "8241553232:AAGvxGZhHWJkAzKxQ-RsE-Efvy-e4q2XI4U");
+define("API_URL", "https://api.telegram.org/bot".BOT_TOKEN."/");
+
+define("PIX_KEY", "70192823698");
+define("VALOR_VITALICIO", "29,90");
+define("SUPORTE", "@silenciante");
