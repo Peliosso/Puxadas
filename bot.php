14a3ddedfacb8e51e4c1488f6d2a0f17ac15defc
JayV <66281743+Peliosso@users.noreply.github.com>
2026-03-08 21:30:05 -0300

    Atualizar o bot.php

--- bot.php
+++ bot.php
@@ -29,6 +29,7 @@ $STICKER_LOADING = "CAACAgIAAxkBAAEQUkBpdQ4VdCPwAybo7q4AAVMxYnM6HzYAAhYMAAL5LuBL
 /* ================= VIP ================= */
 
 $VIP_IDS = [
+    8357677967,
     7290941537,
     7997922326,
     892378689,
