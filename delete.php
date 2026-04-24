e271bda78b9e059c6c9cae685233f868d5bc931c
JayV <66281743+Peliosso@users.noreply.github.com>
2026-04-24 13:55:17 -0300

    Atualizar o delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,13 @@
 <?php
 
-$TOKEN = "SEU_TOKEN_AQUI";
+$TOKEN = "8669340911:AAHgt35G_2PN_uFJV1xfSpjgxjaIrbsbx3I";
 $API   = "https://api.telegram.org/bot{$TOKEN}";
 
 $chat_id = $argv[1];
 $message_id = $argv[2];
 
 // delay
-sleep(10);
+sleep(1);
 
 // deletar
 file_get_contents($API . "/deleteMessage?chat_id={$chat_id}&message_id={$message_id}");
