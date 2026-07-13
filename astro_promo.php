09338d4266673f36516c5011af1b5b6fde1d9c4c
JayV <66281743+Peliosso@users.noreply.github.com>
2026-07-13 17:39:00 -0300

    Atualizar o astro_promo.php

--- astro_promo.php
+++ astro_promo.php
@@ -115,7 +115,7 @@ foreach($grupos as $chat_id => $v){
 // 📤 ENVIA FOTO + MENSAGEM
 $msg = bot("sendPhoto", [
     "chat_id" => $chat_id,
-    "photo" => "https://kommodo.ai/i/25oxjZ6nOdB5b3S4nDvr",
+    "photo" => "https://photos.app.goo.gl/wrbcMi2MRdJxKHPL9",
     "caption" => mensagemPromo(),
     "parse_mode" => "HTML",
     "reply_markup" => $keyboard
